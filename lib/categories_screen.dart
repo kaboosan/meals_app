cdfc4e1fc857efbc0382858dc409fb2efda12ac4
Kamel Bourahla <bourahla.kamel@gmail.com>
2021-02-03 07:18:34 +0100

    Add main screen + main structure of app

---
+++ lib/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:meals_app/category_item.dart';
+
+import 'dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('DeliMeal'),
+        ),
+        body: GridView(
+          children: DUMMY_CATEGORIES
+              .map((cat) => CategoryItem(cat.title, cat.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+        ));
+  }
+}
