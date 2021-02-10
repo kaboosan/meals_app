07e3c93b858a0e71a03e77823bc04c98f0afb35b
Kamel Bourahla <bourahla.kamel@gmail.com>
2021-02-10 05:21:40 +0100

    Add Tab menu (bottom or top)

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,6 +8,53 @@ class TabsSreen extends StatefulWidget {
 }
 
 class _TabsSreenState extends State<TabsSreen> {
+  final List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoritesScreen(), 'title': 'Your Favorite'},
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  //Version with Tab on the bottom
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//Version with Tab on the top
+/*class _TabsSreenState extends State<TabsSreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -34,4 +81,4 @@ class _TabsSreenState extends State<TabsSreen> {
       ),
     );
   }
-}
+}*/
