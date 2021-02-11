8435135131165bb208492e47c0a40adfa5a71a67
Kamel Bourahla <bourahla.kamel@gmail.com>
2021-02-11 06:18:29 +0100

    Add ability to remove menu

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 import '../dummy_data.dart';
 
-class CategoryMealsAcreen extends StatelessWidget {
+class CategoryMealsAcreen extends StatefulWidget {
   static const routeName = '/categories';
-  //final String catId;
-  //final String catTitle;
 
-  //CategoryMealsAcreen(this.catId, this.catTitle);
+  @override
+  _CategoryMealsAcreenState createState() => _CategoryMealsAcreenState();
+}
+
+class _CategoryMealsAcreenState extends State<CategoryMealsAcreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+  bool _loadedInitData = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    if (!_loadedInitData) {
+      final routesArgs =
+          ModalRoute.of(context).settings.arguments as Map<String, String>;
+      categoryTitle = routesArgs['title'];
+      final categoryId = routesArgs['id'];
+      displayedMeals = DUMMY_MEALS.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInitData = true;
+    }
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((meal) => meal.id == mealId);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final routesArgs =
-        ModalRoute.of(context).settings.arguments as Map<String, String>;
-    final categoryTitle = routesArgs['title'];
-    final categoryId = routesArgs['id'];
-    final categoryMeals = DUMMY_MEALS.where((meal) {
-      return meal.categories.contains(categoryId);
-    }).toList();
-
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
@@ -26,14 +51,16 @@ class CategoryMealsAcreen extends StatelessWidget {
       body: ListView.builder(
           itemBuilder: (ctx, index) {
             return MealItem(
-                id: categoryMeals[index].id,
-                title: categoryMeals[index].title,
-                imageUrl: categoryMeals[index].imageUrl,
-                duration: categoryMeals[index].duration,
-                affordability: categoryMeals[index].affordability,
-                complexity: categoryMeals[index].complexity);
+              id: displayedMeals[index].id,
+              title: displayedMeals[index].title,
+              imageUrl: displayedMeals[index].imageUrl,
+              duration: displayedMeals[index].duration,
+              affordability: displayedMeals[index].affordability,
+              complexity: displayedMeals[index].complexity,
+              removeItem: _removeMeal,
+            );
           },
-          itemCount: categoryMeals.length),
+          itemCount: displayedMeals.length),
     );
   }
 }
